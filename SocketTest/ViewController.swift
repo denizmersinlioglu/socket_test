9eaee8a0af62b4e34511859ea6e392e6e5803fb9
Deniz Mersinlioğlu <mersinliogludeniz@gmail.com>
2018-10-02 23:45:26 +0300

    Initial Commit

---
+++ SocketTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SocketTest
+//
+//  Created by Deniz Mersinlioğlu on 2.10.2018.
+//  Copyright © 2018 mersinliogludeniz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
